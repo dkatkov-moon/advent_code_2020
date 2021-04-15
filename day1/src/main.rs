ccbb122d3bd401f9c0904210019803762304b3b3
Denis Katkov <Denis.Katkov@standardbank.co.za>
2021-04-15 21:54:27 +0300

    day1 temp

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> anyhow::Result<()>{
+    let mut input_iter = include_str!("input.txt")
+        .split('\n')
+        .map(str::parse::<i64>)
+        .map(Result::unwrap);
+    dbg!(input_iter.next());
+
+    Ok(())
+}
+
+fn find_pair_2020(numbers : Vec<i64>) -> Option<(i64, i64)> {
+    todo!()
 }
