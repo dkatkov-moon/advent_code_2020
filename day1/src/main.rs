bb8cd52828d0020b317a31a2d407bb25f6d45da0
Denis Katkov <Denis.Katkov@standardbank.co.za>
2021-05-18 20:15:56 +0300

    day 2 upd

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,17 +1,17 @@
 use itertools::Itertools;
 
 fn main() -> anyhow::Result<()>{
-    let (a, b) = include_str!("input.txt")
+    let (a, b, c) = include_str!("input.txt")
         .split('\n')
         .map(str::parse::<i64>)
         .collect::<Result<Vec<_>,_>>()?
         .into_iter()
         .tuple_combinations()
-        .find(|(a, b)| a + b == 2020)
+        .find(|(a, b, c)| a + b + c == 2020)
         .expect("no pair");
 
-    dbg!(a+b);
-    dbg!(a*b);
+    dbg!(a+b+c);
+    dbg!(a*b*c);
 
     Ok(())
 }
