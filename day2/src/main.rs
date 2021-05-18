fef122ab2c1fc9fdca7901d0e9600a6cce65bfe4
Denis Katkov <Denis.Katkov@standardbank.co.za>
2021-05-18 20:16:20 +0300

    day 2 upd

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,4 +1,3 @@
-use std::ops::RangeInclusive;
 
 #[cfg(test)]
 mod tests {
@@ -8,11 +7,12 @@ mod tests {
     fn test_is_valid() {
         let pp = PasswordPolicy {
             byte: b'a',
-            range: 1..=3,
+            positions: [0,2],
         };
 
         assert_eq!(pp.is_valid("zeus"), false);
         assert_eq!(pp.is_valid("abcd"), true);
+        assert_eq!(pp.is_valid("qbad"), true);
         assert_eq!(pp.is_valid("aaaa"), false);
     }
 
@@ -25,91 +25,54 @@ mod tests {
             (
                 PasswordPolicy {
                 byte: b'a',
-                range: 1..=3
+                positions: [0,2]
             },
              "banana")
         );
     }
 }
 
-#[derive(thiserror::Error, Debug)]
-enum ParseError {
-    #[error("expected {0}")]
-    Expected(&'static str),
-}
-
+#[derive(PartialEq, Debug)]
 struct PasswordPolicy {
     byte: u8,
-    range: RangeInclusive<usize>
+    positions: [usize; 2]
 }
 
 impl PasswordPolicy {
     fn is_valid(&self, password: &str) -> bool {
-        self.range.contains(
-            &password.as_bytes()
-            .iter()
-            .copied()
-            .filter(|&b| b == self.byte)
-            .count()
-        )
+        self.positions
+        .iter()
+        .copied()
+        .filter(|&index| password.as_bytes()[index] == self.byte)
+        .count() == 1
     }
 }
 
-use std::fmt::Debug;
+fn parse_line(s: &str) -> anyhow::Result<(PasswordPolicy, &str)> {
+    peg::parser!{
+        grammar parser() for str {
+          rule number() -> usize
+            = n:$(['0'..='9']+) { n.parse().unwrap() }
+      
+          rule position() -> usize
+            = n:number() {n - 1}
 
-impl PartialEq for PasswordPolicy {
-    fn eq(&self, other: &Self) -> bool {
-        self.byte == other.byte && self.range == other.range
-    }
-}
+          rule positions() -> [usize; 2]
+            = first:position() "-" second:position() { [first, second] }
 
-impl Debug for PasswordPolicy {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("PasswordPolicy")
-        .field("byte", &self.byte)
-        .field("range", &self.range)
-        .finish()
-    }
-}
+          rule byte() -> u8
+            = letter:$(['a'..='z']) { letter.as_bytes()[0] }
+            
+          rule password() -> &'input str
+            = letters:$([_]*) { letters }
 
-fn parse_line(line: &str) -> anyhow::Result<(PasswordPolicy, &str)> {
-    let (policy, password) = {
-        let mut tokens = line.split(':');
-        (
-            tokens
-            .next()
-            .ok_or(ParseError::Expected("passowrd policy"))?,
-            tokens
-            .next()
-            .ok_or(ParseError::Expected("password"))?
-            .trim()
-        )
-    };
-
-    let (range, byte) = {
-        let mut tokens = policy.split(' ');
-        (
-            tokens.next().ok_or(ParseError::Expected("policy range"))?,
-            tokens.next().ok_or(ParseError::Expected("policy byte"))?,
-        )
-    };
-
-    let byte = if byte.as_bytes().len() == 1 {
-        byte.as_bytes()[0]
-    } else {
-        return Err(ParseError::Expected("password policy byte to be exact 1 byte").into());
-    };
-
-    let (min, max) = {
-        let mut tokens = range.split('-');
-        (
-            tokens.next().ok_or(ParseError::Expected("policy range (low bound)"))?,
-            tokens.next().ok_or(ParseError::Expected("policy range (up bound)"))?,
-        )
-    };
-
-    let range = (min.parse()?)..=(max.parse()?);
-    Ok((PasswordPolicy{byte, range}, password))
+          pub(crate) rule line() -> (PasswordPolicy, &'input str)
+            = positions:positions() " " byte:byte() ": " password:password() {
+              (PasswordPolicy { byte, positions }, password)
+          }
+        }
+      }
+    Ok(parser::line(s)?)
 }
 
 fn main() -> anyhow::Result<()>{
